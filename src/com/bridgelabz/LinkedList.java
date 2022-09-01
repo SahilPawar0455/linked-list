24b28d1454a02d1239c3b21b72a4d476e008f5e3
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-01 20:41:41 +0530

    ADDED: add the element in linkedlist

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -3,4 +3,24 @@ package com.bridgelabz;
 public class LinkedList<E> {
     Node<E> head;
     Node<E> tail;
+    public void addElement(E key){
+        Node node = new Node<>(key);
+        if (head == null){
+            head = node;
+        }else {
+            node.next = head;
+            head = node;
+        }
+    }
+    public void showLinkedList(){
+        Node currentNode = head;
+        if (head == null){
+            System.out.println("Linked List is Empty");
+        }
+        while (currentNode != null){
+            System.out.print(currentNode.key+" -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+    }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,5 +4,9 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Linked List Program");
         LinkedList linkedList = new LinkedList();
+        linkedList.addElement(70);
+        linkedList.addElement(30);
+        linkedList.addElement(56);
+        linkedList.showLinkedList();
     }
 }
