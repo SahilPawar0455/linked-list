21e0a4b0c091a867c84b1ece641be893f9cade7a
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-01 21:43:08 +0530

    ADDED: Ability to delete the last Element in linked list

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -12,10 +12,14 @@ public class LinkedListMain {
         linkedList1.appending(70);
         linkedList1.appending(30);
         linkedList1.appending(56);
+        linkedList1.appending(75);
+        linkedList1.appending(80);
         linkedList1.showLinkedList();
         linkedList1.insertElementAtPosition(2,5);
         linkedList1.showLinkedList();
         linkedList1.firstElementDelete();
+        linkedList1.showLinkedList();
+        linkedList1.lastElementDelete();
         linkedList1.showLinkedList();
     }
 }
