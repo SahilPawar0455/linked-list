d67dbca93d0e05946ef4580cb6cae064a0f2f938
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-02 12:18:58 +0530

    ADDED: insert the element in appending

---
+++ src/com/bridgelabz/stackandqueue/Queue.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.stackandqueue;
+
+import com.bridgelabz.linkedList.LinkedList;
+
+public class Queue {
+    public static void main(String[] args) {
+        LinkedList queue = new LinkedList();
+        queue.appending(56);
+        queue.appending(30);
+        queue.appending(70);
+        queue.showLinkedList();
+    }
+}
